f678303299112ec8284fa13d64eb7b7b00d9f77a
raneemr05 <57775695+raneemr05@users.noreply.github.com>
2024-05-16 13:12:12 +0100

    New script with logic

---
+++ script.js
@@ -0,0 +1,15 @@
+// Basic code with predefined numbers to calculate the average
+
+// Assign a predefined list of numbers
+let numArray = [40, 60, 10, 20, 80];
+
+// Calculate the sum using a basic loop
+let sum = 0;
+for (let i = 0; i < numArray.length; i++) {
+    sum += numArray[i];
+}
+// Calculate the average
+let average = sum / numArray.length;
+
+// Print the average
+console.log(`The average is: ${average}`);
